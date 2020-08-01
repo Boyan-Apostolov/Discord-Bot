f3e1b2e6de45df12100e74e153d42a33ba30c002
Boyan-Apostolov <boian4934@gmail.com>
2020-08-01 14:18:23 +0300

    Add Bot
    Add Bot

---
+++ DiscordBot/Modules/Commands.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Discord.Commands;
+namespace DiscordBot.Modules
+{
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+        [Command("ping")]
+        public async Task Ping()
+        {
+            await ReplyAsync("pong");
+        }
+    }
+}
